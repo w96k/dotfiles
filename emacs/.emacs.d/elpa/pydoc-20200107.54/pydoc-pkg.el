230f460ae4ab1af988f10d23c0063cc7c94b1ce7
Mikhail Kirillov <w96k@posteo.net>
2020-01-27 19:16:23 +0300

    delete .emacs.d

--- emacs/.emacs.d/elpa/pydoc-20200107.54/pydoc-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/w96k/.emacs.d/elpa/pydoc-20200107.54/pydoc.el  -*- no-byte-compile: t -*-
-(define-package "pydoc" "20200107.54" "functional, syntax highlighted pydoc navigation" 'nil :commit "1dfbab7800827adadb649d6e02c31dd9cd65f0d3" :keywords '("pydoc" "python") :authors '(("John Kitchin" . "jkitchin@andrew.cmu.edu")) :maintainer '("Brian J. Lopes" . "statmobile@gmail.com") :url "https://github.com/statmobile/pydoc")
